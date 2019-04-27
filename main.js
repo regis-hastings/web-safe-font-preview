ef2243064a8f6a3429fb7036e26effc6ebbee772
Regis <25518602+rfjues@users.noreply.github.com>
2019-04-27 14:07:21 -0700

    finished adding event handlers

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).ready(() => {
   
+  $('#text').on('keyup', event => {
+    $('.preview').html($(event.currentTarget).val());
+  });
+  
+  $('#font').change(event => {
+    $('.preview').css('font-family', $(event.currentTarget).val());
+  });
+  
+  $('#weight').change(event => {
+    $('.preview').css('font-weight', $(event.currentTarget).val());
+  });
+  
+  $('#size').keyup(event => {
+    let fontSize = $(event.currentTarget).val() + 'px';
+    $('.preview').css('font-size', fontSize);
+  });
+  
 })
